0da062f598f67d964fe204c08f2195653545ef00
Sami Hindi <lolipop66611@gmail.com>
2023-04-19 10:55:01 +0200

    Added search mechanism

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -11,6 +11,7 @@ const Dashboard = () => {
     const [currentTab, setCurrentTab] = useState('student')
     const [content, setContent] = useState('')
     const [count, setCount] = useState(0)
+    const { search, setSearch } = useContext(SearchContext)
     const { user } = useContext(UserContext)
     const router = useRouter()
 
@@ -21,8 +22,72 @@ const Dashboard = () => {
     }, []);
 
     useEffect(() => {
+        if (search) {
+            let results = students.filter((student) => {
+                return student.name.toLowerCase().includes(search.toLowerCase())
+            })
 
-    })
+            setContent(
+                <div className="overflow-x-auto w-full">
+                    <table className="table w-full">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Klasse</th>
+                                <th>E-Mail</th>
+                                <th>Absenzen</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {results.map((student) => (
+                                <tr key={student.name}>
+                                    <td>
+                                        <Link className="flex items-center space-x-3" href={`/student/${student.email}`}>
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle w-12 h-12">
+                                                    <img src={student.avatar} alt={student.name} />
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <div className="font-bold">{student.name}</div>
+                                                <div className="text-sm opacity-50">Switzerland</div>
+                                            </div>
+                                        </Link>
+                                    </td>
+                                    <td>
+                                        {student.class}
+                                        <br />
+                                        <span className="badge badge-ghost badge-sm">{student.teacher}</span>
+                                    </td>
+                                    <td>{student.email}</td>
+                                    <td>
+                                        {
+                                            student.absences_details > 40 ? (
+                                                <span className="badge badge-error">{student.absences}</span>
+                                            ) : (
+                                                <span className="badge badge-success">{student.absences}</span>
+                                            )
+                                        }
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Class</th>
+                                <th>Email</th>
+                                <th></th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            )
+
+        }
+    }, [search])
 
     useEffect(() => {
         let absenceCount = 0
